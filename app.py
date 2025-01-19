67ab616bdfb302743139381c61a0449a54924da2
Kushal Dhangar <155418668+KushalDhangar@users.noreply.github.com>
2025-01-19 07:57:10 +0000

    Added the Flask Project

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask # Flask is class and flask is a module
+
+app=Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "hello ,Kushal"
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0',debug=True)
